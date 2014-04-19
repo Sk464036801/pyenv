4359cb8a0fd3f1e0345735b66f6a1bc56fba95bd
Yamashita Yuu <yamashita@geishatokyo.com>
2014-04-19 17:17:48 +0900

    Remove useless files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pyenv):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pyenv" command="/usr/bin/pyenv"
--- debian/pyenv.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pyenv package
-#
-0 4	* * *	root	[ -x /usr/bin/pyenv_maintenance ] && /usr/bin/pyenv_maintenance
